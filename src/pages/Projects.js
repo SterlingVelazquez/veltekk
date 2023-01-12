52d76b8caea73a0bca4d275f5e24cd86cd790b43
Sterling Velazquez <SterlingVelazquez@users.noreply.github.com>
2023-01-11 19:40:29 -0500

    Edited Wedding Link Again

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -52,7 +52,7 @@ class Projects extends React.Component {
                   support coming soon.</p>
               </li>
             </a>
-            <a className="projectLink" target="_blank" rel="noopener noreferrer" href="https://www.paoandcamilowedding.vercel.app/">
+            <a className="projectLink" target="_blank" rel="noopener noreferrer" href="https://paoandcamilowedding.vercel.app/">
               <li className="projectItem">
                 <p className="projectHeader">Paola &#38; Camilo's Wedding</p>
                 <p className="projectDescription">A wedding website which leverages React effects to create smooth page animations on scroll. This website, and all the following,
